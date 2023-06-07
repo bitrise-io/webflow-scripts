183accb73b179102c18b7722334af72d6485f17e
Antal Orcsik <aorcsik@gmail.com>
2023-06-07 22:35:19 +0200

    improve local server

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  fetch(process.argv[2] + req.url).then(response => {
-    res.statusCode = response.status;
-    res.setHeader('Content-Type', response.headers.get("Content-Type"));
-    return response.text();
-  }).then(text => {
-    res.end(text);
-  });
+  const url = new URL("http://" + req.hostname + req.url);
+  const filePath = '.' + url.pathname;
+  
+  fs.readFile(filePath, (error, content) => {
+      if (!error) {
+
+        res.statusCode = 200;
+        const extname = path.extname(filePath);
+        if (extname == ".js") res.setHeader('Content-Type', "text/javascript");
+        if (extname == ".html") res.setHeader('Content-Type', "text/html");
+        res.end(content);
+  
+      } else {
+        console.log(error);
+
+        fetch(process.argv[2] + req.url).then(response => {
+          res.statusCode = response.status;
+          res.setHeader('Content-Type', response.headers.get("Content-Type"));
+          return response.text();
+        }).then(text => {
+          text = text.replace("https://bitrise-io.github.io/webflow-scripts/", "/docs/");
+      
+          res.end(text);
+        });
+  
+      }
+    
+    });
+
 });
 
 server.listen(port, hostname, () => {
