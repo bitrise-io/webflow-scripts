91f3c2a834907b8b931cf9ead6dbecd442a82725
Antal Orcsik <aorcsik@gmail.com>
2023-06-08 15:00:34 +0200

    cleanup local dev server code

--- index.js
+++ index.js
@@ -10,31 +10,26 @@ const server = http.createServer((req, res) => {
   const filePath = '.' + url.pathname;
   
   fs.readFile(filePath, (error, content) => {
-      if (!error) {
+    if (!error) {
 
-        res.statusCode = 200;
-        const extname = path.extname(filePath);
-        if (extname == ".js") res.setHeader('Content-Type', "text/javascript");
-        if (extname == ".html") res.setHeader('Content-Type', "text/html");
-        res.end(content);
-  
-      } else {
-        console.log(error);
+      res.statusCode = 200;
+      const extname = path.extname(filePath);
+      if (extname == ".js") res.setHeader('Content-Type', "text/javascript");
+      if (extname == ".html") res.setHeader('Content-Type', "text/html");
+      res.end(content);
 
-        fetch(process.argv[2] + req.url).then(response => {
-          res.statusCode = response.status;
-          res.setHeader('Content-Type', response.headers.get("Content-Type"));
-          return response.text();
-        }).then(text => {
-          text = text.replace("https://bitrise-io.github.io/webflow-scripts/", "/docs/");
-      
-          res.end(text);
-        });
-  
-      }
-    
-    });
+    } else {
 
+      fetch(process.argv[2] + req.url).then(response => {
+        res.statusCode = response.status;
+        res.setHeader('Content-Type', response.headers.get("Content-Type"));
+        return response.text();
+      }).then(text => {
+        res.end(text.replace("https://bitrise-io.github.io/webflow-scripts/", "/docs/"));
+      });
+
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
