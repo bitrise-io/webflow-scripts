52ab58b4bddee790edde959b090ab0ac5964bd74
Antal Orcsik <antal.orcsik@bitrise.io>
2023-06-19 14:32:36 +0200

    FRONTEND-289 turn off lever service

--- src/js/careers.js
+++ src/js/careers.js
@@ -1,24 +1,9 @@
 import BambooHRService from "./careers/BambooHRService";
 import JobDepartmentList from "./careers/JobDepartmentList";
-import LeverService from "./careers/LeverService";
-
-const url = new URL(document.location.href);
-const test = url.searchParams.get("test");
 
 const jobDepartmentList = new JobDepartmentList();
 
-if (test == null) {
-
-  const lever = new LeverService();
-  lever.getJobs().then(jobDepartments => {
-    jobDepartmentList.render(jobDepartments);
-  });    
-
-} else {
-
-  const bamboo = new BambooHRService();
-  bamboo.getJobs().then(bambooJobs => {
-    jobDepartmentList.render(bambooJobs);
-  });
-
-}
+const bamboo = new BambooHRService();
+bamboo.getJobs().then(jobs => {
+  jobDepartmentList.render(jobs);
+});
