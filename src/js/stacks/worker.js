fdcdce040d43fab65de10d187a9c1d274d778ac3
Antal Orcsik <antal.orcsik@bitrise.io>
2024-06-17 11:17:28 +0200

    update stacks worker to handle rss feeds

--- src/js/stacks/worker.js
+++ src/js/stacks/worker.js
@@ -4,6 +4,19 @@ export default {
 
     urlObject.hostname = 'webflow.bitrise.io';
 
+    const rssMatch = urlObject.pathname.match(/^\/stacks\/(.*index\.xml)/);
+    if (rssMatch) {
+      urlObject.hostname = 'stacks.bitrise.io';
+      urlObject.pathname = `/${rssMatch[1]}`;
+      const response = await fetch(urlObject);
+      const data = (await response.text()).replaceAll('stacks.bitrise.io/', 'bitrise.io/stacks/');
+      return new Response(data, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: Object.fromEntries(response.headers.entries()),
+      });
+    }
+
     if (urlObject.pathname.match(/^\/stacks\/.+\/.+/)) {
       urlObject.pathname = '/stacks/subpage';
     } else if (urlObject.pathname.match(/\/stacks$|stacks\/(.*)/)) {
