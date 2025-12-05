e6115b268cdcfdbe51d56bbc548930dd579157ea
Antal Orcsik <antal.orcsik@bitrise.io>
2025-12-05 17:22:47 +0100

    improve changelog-proxy worker

--- src/js/changelog/worker.js
+++ src/js/changelog/worker.js
@@ -1,52 +1,51 @@
-/**
- *
- * @param {Promise<Response>} originalResponse
- * @returns {Promise<Response>}
- */
-async function addCorsHeaders(originalResponse) {
-  let response = await originalResponse;
-  // Recreate the response so you can modify the headers
+export default {
+  async fetch(request) {
+    const urlObject = new URL(request.url);
 
-  response = new Response(response.body, response);
-  // Set CORS headers
+    let useCors = false;
+    let transformBody = null;
+    urlObject.hostname = 'webflow.bitrise.io';
 
-  response.headers.set('Access-Control-Allow-Origin', '*');
+    if (urlObject.pathname.match(/^\/changelog\/api\/(.+\.json)$/)) {
+      urlObject.hostname = 'discuss.bitrise.io';
+      urlObject.pathname = urlObject.pathname.replace(/^\/changelog\/api\/(.+\.json)$/, '/$1');
+      useCors = true;
+    } else if (urlObject.pathname.match(/^\/changelog\.json$/)) {
+      urlObject.hostname = 'web-cdn.bitrise.io';
+      urlObject.pathname = 'changelog.json';
+      useCors = true;
+    } else if (urlObject.pathname.match(/^\/changelog_latest\.json$/)) {
+      urlObject.hostname = 'web-cdn.bitrise.io';
+      urlObject.pathname = 'changelog_latest.json';
+      useCors = true;
+    } else if (urlObject.pathname.match(/^\/changelog\.xml$/)) {
+      urlObject.hostname = 'web-cdn.bitrise.io';
+      urlObject.pathname = 'changelog.xml';
+    } else if (urlObject.pathname.match(/^\/changelog\/.+/)) {
+      const originalPath = urlObject.pathname;
+      urlObject.pathname = '/changelog/topic';
+      transformBody = (data) => data.replace('https://bitrise.io/changelog/topic', `https://bitrise.io${originalPath}`);
+    }
 
-  // Append to/Add Vary header so browser will cache response correctly
-  response.headers.append('Vary', 'Origin');
+    if (!useCors && !transformBody) {
+      return fetch(urlObject);
+    }
 
-  return response;
-}
+    let response = await fetch(urlObject);
+    const responseBody = await response.text();
+    response = new Response(responseBody, response);
 
-addEventListener('fetch', (event) => {
-  const urlObject = new URL(event.request.url);
-  let useCors = true;
+    if (transformBody) {
+      response.body = transformBody(responseBody);
+    }
 
-  urlObject.hostname = 'webflow.bitrise.io';
+    if (useCors) {
+      // Set CORS headers
+      response.headers.set('Access-Control-Allow-Origin', '*');
+      // Append to/Add Vary header so browser will cache response correctly
+      response.headers.append('Vary', 'Origin');
+    }
 
-  if (urlObject.pathname.match(/^\/changelog\/api\/(.+\.json)$/)) {
-    urlObject.hostname = 'discuss.bitrise.io';
-    urlObject.pathname = urlObject.pathname.replace(/^\/changelog\/api\/(.+\.json)$/, '/$1');
-    useCors = true;
-  } else if (urlObject.pathname.match(/^\/changelog\.json$/)) {
-    urlObject.hostname = 'web-cdn.bitrise.io';
-    urlObject.pathname = 'changelog.json';
-    useCors = true;
-  } else if (urlObject.pathname.match(/^\/changelog_latest\.json$/)) {
-    urlObject.hostname = 'web-cdn.bitrise.io';
-    urlObject.pathname = 'changelog_latest.json';
-    useCors = true;
-  } else if (urlObject.pathname.match(/^\/changelog\.xml$/)) {
-    urlObject.hostname = 'web-cdn.bitrise.io';
-    urlObject.pathname = 'changelog.xml';
-    useCors = true;
-  } else if (urlObject.pathname.match(/^\/changelog\/.+/)) {
-    urlObject.pathname = '/changelog/topic';
-  }
-
-  if (useCors) {
-    event.respondWith(addCorsHeaders(fetch(urlObject)));
-  } else {
-    event.respondWith(fetch(urlObject));
-  }
-});
+    return response;
+  },
+};
