8193a2a1176670e8d218ff7a330a1e591c18f301
Antal Orcsik <antal.orcsik@bitrise.io>
2025-12-05 11:00:27 +0100

    improve wrangler deploy script

--- deploy_worker.sh
+++ deploy_worker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cp -rf src/js/$1/wrangler.toml ./
+CONFIG_PATH=src/js/$1/wrangler.toml
 
 shift 1
-
-npx wrangler deploy $@
-
-rm ./wrangler.toml
+if npx wrangler whoami 2>&1 | grep -q "You are not authenticated"; then
+  npx wrangler login
+fi
+npx wrangler deploy --config $CONFIG_PATH $@
