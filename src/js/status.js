94069fe2defb337bf5d4b6b3b929eddbdbcd892c
Antal Orcsik <antal.orcsik@bitrise.io>
2026-03-25 13:15:23 +0100

    status and 404 page refactor

--- src/js/status.js
+++ src/js/status.js
@@ -1,44 +1,8 @@
-/**
- * Statuspage Embed Link
- * @see https://metastatuspage.com/api
- */
-
 import '../css/status.css';
+import { renderStatus } from './shared/status';
 
-/**
- * @typedef {{
- *  page: {
- *    id: string;
- *    name: string;
- *    url: string;
- *    time_zone: string;
- *    updated_at: string;
- *  };
- *  status: {
- *    indicator: "minor" | "major" | "critical" | "none" | "maintenance";
- *    description: string;
- *  };
- * }} StatusResult
- */
-
-(async () => {
-  /** @type {HTMLAnchorElement} */
-  const statusLink = document.getElementById('statuspage');
-  statusLink.className = `statuspage__entry-point`;
-
-  const statusURL = new URL(statusLink.href);
-  statusURL.pathname = '/api/v2/status.json';
-
-  try {
-    const result = await fetch(statusURL);
-    /** @type {StatusResult} */
-    const data = await result.json();
-    const severity = data.status.indicator === 'maintenance' ? 'minor' : data.status.indicator;
-    statusLink.className = `statuspage__entry-point statuspage__severity-${severity}`;
-    statusLink.innerHTML = `Status: ${data.status.description}`;
-  } catch (e) {
-    /* something's wrong */
-  }
-})();
+window.addEventListener('load', async () => {
+  await renderStatus(document.getElementById('statuspage'));
+});
 
 if (import.meta.webpackHot) import.meta.webpackHot.accept();
